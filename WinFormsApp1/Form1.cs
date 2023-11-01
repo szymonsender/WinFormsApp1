d83bb44858b2c5c3dbf959de2efec6243b1c85c0
szymonsender <senderszymom@gmail.com>
2023-11-01 18:48:14 +0100

    Bubble sort i insert sort

--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -68,5 +68,60 @@ namespace WinFormsApp1
         {
 
         }
+
+
+        private int[] BubbleSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 0; i < length - 1; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+
+        private int[] InsertionSort(int[] arr)
+        {
+            int length = arr.Length;
+            for (int i = 1; i < length; i++)
+            {
+                int key = arr[i];
+                int j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+                }
+                arr[j + 1] = key;
+            }
+            return arr;
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
